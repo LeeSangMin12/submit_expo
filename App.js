b0227ec39b07673609e41d9c50da59056ed39be8
DESKTOP-IG5B1I6\isang <isangmin516@gmail.com>
2023-06-05 02:37:44 +0900

    firebase 제거하고 앱 빌드 테스트

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import { Ionicons } from '@expo/vector-icons'
 import { Fontisto } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { initializeApp } from "firebase/app";
 
 import Toast from 'react-native-toast-message';
 import store from '@/store/store'
@@ -36,18 +35,6 @@ import community_active from '@/assets/img/bottom_tab/community_active.png'
 import my_active from '@/assets/img/bottom_tab/my_active.png'
 
 import success_check from '@/assets/img/icon/success_check.png'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyALdHHjwbdCeG3f991AKTmDK9RFoW92bAM",
-  authDomain: "submit-75715.firebaseapp.com",
-  projectId: "submit-75715",
-  storageBucket: "submit-75715.appspot.com",
-  messagingSenderId: "838876333900",
-  appId: "1:838876333900:web:b172ecdb032ee81e35b1e9",
-  measurementId: "G-M9KKYZ1YV7"
-};
-
-const app = initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 const Bottom_tab = createBottomTabNavigator();
@@ -134,7 +121,7 @@ const App = () => {
           },
         }}>
         <Stack.Navigator>
-          {/* <Stack.Screen name="Login_page" component={Login_page} options={{ headerShown: false }} />
+          <Stack.Screen name="Login_page" component={Login_page} options={{ headerShown: false }} />
           <Stack.Screen
             name="회원가입"
             options={({ navigation }) => ({
@@ -158,7 +145,7 @@ const App = () => {
                 page_count={page_count}
                 set_page_count={set_page_count}
               />)}
-          </Stack.Screen> */}
+          </Stack.Screen>
           <Stack.Screen name="Bottom_navigation" component={Bottom_navigation} options={{ headerShown: false }} />
           <Stack.Screen
             name="과제 등록"
