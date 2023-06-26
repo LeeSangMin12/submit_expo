c543f1ea081f987e7062ba7c7dfaa286912fd5d0
DESKTOP-IG5B1I6\isang <isangmin516@gmail.com>
2023-06-26 22:01:55 +0900

    정보 수정시 닉네임 검사

--- src/pages/my/user_card/edit_profile/Edit_profile.js
+++ src/pages/my/user_card/edit_profile/Edit_profile.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useState } from "react";
 import { View, StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { exec_request } from "@/shared/js/api";
 import COLORS from '@/shared/js/colors';
@@ -9,6 +10,7 @@ import Set_nickname from '@/components/setting/Set_nickname';
 import Set_image from '@/components/setting/Set_image';
 
 const Edit_profile = () => {
+  const navigation = useNavigation();
   const {
     img_url,
     nickname,
@@ -18,7 +20,62 @@ const Edit_profile = () => {
     img_url: img_url,
     nickname: nickname,
   });
+  const [err_nickname, set_err_nickname] = useState('');
 
+  /**
+   * 닉네임 검사 후 유저 정보를 수정
+   */
+  const edit_user_info = async () => {
+    if (await check_nickname()) {
+      console.log('이 닉네임은 정상이네요.');
+    }
+  }
+
+  /**
+   * nickname 값이 올바른지 검사
+   */
+  const check_nickname = async () => {
+    if (user_input.nickname.length < 2) {
+      set_err_nickname('2글자 이상 입력해주세요.');
+      return false;
+    } else if (!await api_check_duplicate_check_nickname()) {
+      set_err_nickname('중복된 닉네임입니다.');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * 유저 닉네임 검사
+   */
+  const api_check_duplicate_check_nickname = async () => {
+    const params = {
+      url: "check/duplicate_check_nickname",
+      nickname: user_input.nickname
+    };
+
+    const result = await exec_request(params, navigation);
+
+    if (result.status === "ok") {  //중복닉네임 없음
+      return true;
+    } else {  //중복닉네임 존재
+      return false;
+    }
+  }
+
+  /**
+   * 유저 정보 수정
+   */
+  const api_user_edit_info = async () => {
+    const params = {
+      url: "user/edit_info",
+      img_url: user_input.img_url,
+      nickname: user_input.nickname
+    };
+
+    const result = await exec_request(params, navigation);
+  }
 
   return (
     <View style={styles.container}>
@@ -36,7 +93,7 @@ const Edit_profile = () => {
           <Set_image img_url={user_input.img_url} set_value={set_user_input} />
 
           <View style={styles.set_nickname_container}>
-            <Set_nickname nickname={user_input.nickname} set_value={set_user_input} />
+            <Set_nickname nickname={user_input.nickname} err_nickname={err_nickname} set_value={set_user_input} />
           </View>
 
         </ScrollView>
@@ -46,8 +103,7 @@ const Edit_profile = () => {
         <Button
           title="완료하기"
           style={styles.btn_next}
-        // on_press={() => set_page_count(page_count + 1)}
-        // disabled={btn_next_disabled} 
+          on_press={edit_user_info}
         />
       </View>
     </View >
