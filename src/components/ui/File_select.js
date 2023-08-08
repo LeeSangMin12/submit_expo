5c2ae6e9e55d89cce182ac23043760e0afdae37f
DESKTOP-IG5B1I6\isang <isangmin516@gmail.com>
2023-08-08 11:29:59 +0900

    파잂  - 용량이 너무 클때, 안담기는 파일 일때 에러처리

--- src/components/ui/File_select.js
+++ src/components/ui/File_select.js
@@ -1,6 +1,7 @@
-import { Text, View, StyleSheet, Pressable, Linking } from 'react-native';
+import { Text, View, StyleSheet, Pressable, Linking, Alert } from 'react-native';
 import { Fontisto, MaterialIcons, Ionicons } from '@expo/vector-icons';
 import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
 
 import COLORS from '@/shared/js/colors';
 
@@ -21,6 +22,12 @@ const File_select = ({
     try {
       const file = await DocumentPicker.getDocumentAsync();
       if (file.type === 'success') {
+        const file_error_message = await handle_file_errors(file);
+        if (file_error_message !== false) {
+          Alert.alert(file_error_message);
+          return;
+        }
+
         const new_file = {
           name: file.name,
           size: file.size,
@@ -35,6 +42,17 @@ const File_select = ({
     } catch (error) {
       console.log('Error selecting file:', error);
     }
+  };
+
+  const handle_file_errors = async (file) => {
+    const file_info = await FileSystem.getInfoAsync(file.uri);
+    const file_size_mb = file_info.size / (1024 * 1024); // 파일 크기를 메가바이트로 변환.
+
+    const file_error_message =
+      file_info.isDirectory === true ? '해당 파일유형은 사용할 수 없습니다.' :
+        file_size_mb > 50 ? '파일의 용량이 너무 큽니다.' : false;
+
+    return file_error_message;
   };
 
   const de_select_file = (file_num) => {
